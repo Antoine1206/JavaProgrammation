81af91f365d6c49e806ab08ef8cb5c2824678572
Antoine Melanger <antoine.melanger@laposte.net>
2022-03-17 17:00:53 +0100

    -suite exos

---
+++ src/main/java/section11/AutreMain.java
@@ -0,0 +1,22 @@
+package section11;
+
+import java.util.Scanner;
+
+public class AutreMain {
+
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 12 ; i++){
+            System.out.println("Entrer le chiffre : " + (i-1));
+            scanner.nextInt();
+        }
+
+
+
+
+    }
+
+
+}
